457d06108b82b61123a67502487211d46061d146
Shawn Oberndorfer <obesr2008@icloud.com>
2025-08-25 15:03:51 +0000

    Made M1T1 in class to greet a user.

---
+++ M1T1_Hello_User.cpp
@@ -0,0 +1,21 @@
+/*
+M1T1 - Hello User
+shawn-oberndorfer, 8/25/25
+
+INPUT: run the program
+PROCESS: None yet
+OUTPUT: return 0 (no error)
+SPECIAL FX: Greet the user.
+*/
+
+// magic start words
+#include <iostream>
+using namespace std;
+
+int main(){
+    //SFX: Greet User
+    cout << "Hello, user" << endl;
+
+    //output
+    return 0; // no errors
+}
