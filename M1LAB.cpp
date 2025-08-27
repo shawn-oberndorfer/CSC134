1163dbedaddc859afe724201a4958fcbdc2f48e5
Shawn Oberndorfer <obesr2008@icloud.com>
2025-08-27 14:53:37 +0000

    Added more variables and outputs.

--- M1LAB.cpp
+++ M1LAB.cpp
@@ -10,13 +10,21 @@ Pokemon Card Sales Program
 using namespace std;
 
 int main(){
-string item      = "Pokemon";
-int    nub_items = 1000;
-double cost_per  = 5.99;
+string item_name    = "Pokemon Card";
+int    num_items    = 1000;
+int    wanted_items = 0;
+double cost_per     = 5.99;
 
-cout << "Welcome to the " << item << " store!" << endl;
+cout << "Welcome to the " << item_name << " store!" << endl;
+cout << "Each " << item_name << " is $" << cost_per << endl;
+cout << "We have " << num_items << " cards in stock." << endl;
+cout << "How many " << item_name << "s would you like to buy?" << endl;
+cin >> wanted_items;
+
+double total_cost = wanted_items * cost_per;
+
+cout << "if you were to buy " << wanted_items << " " << item_name << "s your total cost would be $" << total_cost << endl;
 
 
 return 0;
-
 }
