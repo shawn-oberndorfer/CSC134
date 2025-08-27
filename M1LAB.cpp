d436af20b642be62b81b899509d59a92b9dd9fbb
Shawn Oberndorfer <obesr2008@icloud.com>
2025-08-27 14:32:46 +0000

    Added first Output

---
+++ M1LAB.cpp
@@ -0,0 +1,20 @@
+/*
+CSC 134
+M1LAB
+Shawn Oberndorfer
+8/27/2025
+Pokemon Card Sales Program
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+string item = "Pokemon";
+
+cout << "Welcome to the " << item << " store!" << endl;
+
+
+return 0;
+
+}
