72686e84c5c7a12fdf3f44e0e76ee95cd867e98d
Shawn Oberndorfer <obesr2008@icloud.com>
2025-08-27 15:36:52 +0000

    Added values to the variables

--- M1HW1.cpp
+++ M1HW1.cpp
@@ -9,9 +9,11 @@ Movie Talk
 using namespace std;
 
 int main(){
-string movie_name   = "";
-int    release_year = 0;
-double gross_income = 0.0;
+string movie_name   = "Cars";
+int    release_year = 2006;
+double gross_income = 461.98;
+
+ cout << "The movie " << movie_name << " came out in the year " << release_year << " and grossed roughly " << gross_income << " million dollars in the box office." << endl;
 
 return 0;
 }
