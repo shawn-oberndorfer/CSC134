02b31bc043a3d4906950d14246efb2a8dc48aa9a
Shawn Oberndorfer <obesr2008@icloud.com>
2025-08-27 15:48:56 +0000

    Added Several quotes and facts

--- M1HW1.cpp
+++ M1HW1.cpp
@@ -13,7 +13,13 @@ string movie_name   = "Cars";
 int    release_year = 2006;
 double gross_income = 461.98;
 
- cout << "The movie " << movie_name << " came out in the year " << release_year << " and grossed roughly " << gross_income << " million dollars in the box office." << endl;
+cout << "The movie " << movie_name << " came out in the year " << release_year << " and grossed roughly " << gross_income << " million dollars in the box office." << endl;
+cout << movie_name << " is a film about a talking racecar who gets lost in a small town and learns about the values of friendship and hard work." << endl;
+cout << "One of the most famous quotes from the film is Lightning McQueen's \"Kachow\". " << endl;
+cout << "I also love the quote from Lightning Mcqueen \"float like a catalac sting like a beaver\"." << endl;
+cout << "One of my favorite scenes from the film was where Lightning is racing Doc down a dirt track and losses miserably by crashing into a cactus after talking smack." << endl;
 
-return 0;
+
+
+return 0;              
 }
