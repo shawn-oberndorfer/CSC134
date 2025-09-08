9768232f7d53e38f3bfa0e56104b562570ab2c71
Shawn Oberndorfer <obesr2008@icloud.com>
2025-09-08 14:34:29 +0000

    Added more detailed description and fixed some grammar.

--- M2LAB1.cpp
+++ M2LAB1.cpp
@@ -3,8 +3,8 @@ CSC 134
 M2LAB
 Shawn Oberndorfer
 Start Date 9/8/2025
-
-
+Crate Calculator
+A program that will take the dimenions of a crate and find its cost to make, sale price, and profit margin based off of set variables.
 */
 #include <iostream>
 using namespace std;
@@ -21,11 +21,11 @@ int main(){
     double crate_sale_price      = 0;
     double crate_profit         = 0;
 
-    cout << "What is the height of you crate?" << endl;
+    cout << "What is the height of you crate in feet?" << endl;
     cin >> crate_height;
-    cout << "What is the depth of your crate?" << endl;
+    cout << "What is the depth of your crate in feet?" << endl;
     cin >> crate_depth;
-    cout << "What is the width of your crate?" << endl;
+    cout << "What is the width of your crate in feet?" << endl;
     cin >> crate_width;
 
     crate_volume = crate_height * crate_depth * crate_width;
