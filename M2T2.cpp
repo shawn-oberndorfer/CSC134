8f8aa234a9b5603a16c2b5c689dcc5c0db2e380d
Shawn Oberndorfer <obesr2008@icloud.com>
2025-09-08 15:39:40 +0000

    Added some dollar signa and rounded the total off.

--- M2T2.cpp
+++ M2T2.cpp
@@ -7,24 +7,29 @@ Recipt Program
 This program prints a recipt for a simple meal.
 */
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main(){
 
     const double meal_price  = 5.99;
     const double tax_percent = 0.08;
-    double tax_amount  = 0;
-    double total_cost  = 0;
+
+    double tax_amount         = 0;
+    double total_cost         = 0;
+    double total_cost_rounded = 0;
     
     tax_amount = meal_price * tax_percent;
     total_cost = meal_price + tax_amount;
 
+    total_cost_rounded = round(total_cost * 100.0) / 100.0;
 
-    cout << "Subtotal: " << meal_price << endl;
+
+    cout << "Subtotal: $" << meal_price << endl;
     cout << endl;
-    cout << "Tax 1: " << tax_percent << endl;
+    cout << "Tax 1: $" << tax_percent << endl;
     cout << endl;
-    cout << "Total: " << total_cost << endl;
+    cout << "Total: $" << total_cost_rounded << endl;
 
     
     
