c80ab9c7037dd6ff6c41060a8d198ed3b31e9cf4
Shawn Oberndorfer <obesr2008@icloud.com>
2025-09-15 14:57:51 +0000

    Updated crate function and added more empty functions.

--- M2HW1.cpp
+++ M2HW1.cpp
@@ -1,9 +1,9 @@
 /*
 CSC 134
-M2HW1 Q1 - Gold
+M2HW1 - Gold
 Shawn Oberndorfer
 Start Date 9/10/25
-Last Worked On 9/10/2025
+Last Worked On 9/15/2025
 */
 #include <iostream>
 #include <cmath>
@@ -24,26 +24,40 @@ int main() {
     } else if(programSelect = 2){
         updatedCrates();
         ending();
+    } else if(programSelect = 3){
+        pizzaParty();
+        ending();
+    } else if(programSelect = 4){
+        cheer();
+        ending();
+    } else {
+        altWelcome();
     }
 
+
     }while (programRunAgain = 'Y');
-   
-    
-
-    
-
     return 0;
 }
-void welcome() {
-    cout << "Welcome what program are you trying to access?" << endl;
+void altWelcome(){
+    cout << "The number you entered is invalid please enter a number 1-4." << endl;
     cout << "1. Bank Account" << endl;
-    cout << "2." << endl;
-    cout << "3." << endl;
-    cout << "4." << endl;
+    cout << "2. Crate Calculator" << endl;
+    cout << "3. Pizza Calculator" << endl;
+    cout << "4. Cheer Program" << endl;
+}
+void welcome() {
+    cout << "What program are you trying to access?" << endl;
+    cout << "1. Bank Account" << endl;
+    cout << "2. Crate Calculator" << endl;
+    cout << "3. Pizza Calculator" << endl;
+    cout << "4. Cheer Program" << endl;
     cin >> programSelect;
 }
 void ending(){
     programRunAgain = 'N';
+    cout << endl;
+    cout << endl;
+    cout << endl;
     cout << "Would you like to start another program? (Y/N)" << endl;
     cin >> programRunAgain;
     programRunAgain = toupper(programRunAgain);
@@ -105,5 +119,41 @@ void bankAccount() {
 }
 
 void updatedCrates(){
+
+    const double COST_PER_CUBIC_FOOT   = 0.3;
+    const double CHARGE_PER_CUBIC_FOOT = 0.52;
+
+    double crate_height          = 0;
+    double crate_depth           = 0;
+    double crate_width           = 0;
+    double crate_volume          = 0;
+    double crate_production_cost = 0;
+    double crate_sale_price      = 0;
+    double crate_profit          = 0;
+
+    cout << "What is the height of you crate in feet?" << endl;
+    cin >> crate_height;
+    cout << "What is the depth of your crate in feet?" << endl;
+    cin >> crate_depth;
+    cout << "What is the width of your crate in feet?" << endl;
+    cin >> crate_width;
+
+    crate_volume = crate_height * crate_depth * crate_width;
+    crate_production_cost = crate_volume * COST_PER_CUBIC_FOOT;
+    crate_sale_price = crate_volume * CHARGE_PER_CUBIC_FOOT;
+    crate_profit = crate_sale_price - crate_production_cost;
+
+    cout << "Each crate has a volume of " << crate_volume << " cubic feet." << endl;
+    cout << "The cost to make each crate is $" << crate_production_cost << "." << endl;
+    cout << "The sale price for each crate is $" << crate_sale_price << "." << endl;
+    cout << "The profit per crate sold would be $" << crate_profit << "." << endl;
+
+}
+
+void pizzaParty(){
+
+}
+
+void cheer(){
 
 }
