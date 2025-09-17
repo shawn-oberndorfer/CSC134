374a60693fd4efd718143f567f40b13d4ddeb4f5
Shawn Oberndorfer <obesr2008@icloud.com>
2025-09-17 15:25:22 +0000

    Finished M2BONUS

--- M2BONUS.cpp
+++ M2BONUS.cpp
@@ -1,12 +1,10 @@
 /*
-1. Given a rectangle with height 8 and width 10, calculate and print the area. (This one is most similar to "Apple Orchard", except these are both of type int. The formula "Area equals length times width" will be useful.)
-2. Assume a product is sold for $99.99, and sales tax is 8%. Display the product price, the amount of sales tax, and then the final cost including sales tax. (For handling monetary values, and percentages, you will want to use the double type. "Percent" is "per cent", meaning a 8% sales tax in numerical terms is 0.08 )
-3. Assume a pizza is 12 inches in diameter. Using the standard formula, calculate and display the area of that pizza. (You'll need to determine what common geometrical shape best fits a pizza, then look up the formula for area for that shape.)
 4. If the pizza from question 3 is cut into 8 pieces, what is the area of each piece?
 As usual, upload a zip containing your source code as well as a screenshot of your running program. Your file should be named in the format M1BONUS_Lastname.
 */
 #include <iostream>
 #include <iomanip>
+#include <cmath>
 using namespace std;
 
 int main() {
@@ -14,7 +12,7 @@ int main() {
     //Variables
     int recHeight = 8, recWidth = 10, recArea;
     double productCost = 99.99, productTax, taxPercent = 0.08, finalCost;
-    double pizzaDiameter = 12, pizzaArea;
+    double pizzaDiameter = 12, pizzaArea, pizzaRadius, sliceArea;
     
     cout << "-----Problem 1-----" << endl;
     recArea = recHeight * recWidth;
@@ -27,6 +25,15 @@ int main() {
     cout << "Subtotal: " << productCost << endl;
     cout << "Tax:      " << productTax << endl;
     cout << "Total:    " << finalCost << endl;
+    cout << endl;
+    cout << "-----Problem 3-----" << endl;
+    pizzaRadius = pizzaDiameter / 2;
+    pizzaArea = pizzaRadius * pizzaRadius * M_PI;
+    cout << "The area of the pizza is " << pizzaArea << "." << endl;
+    cout << endl;
+    cout << "-----Problem 4 -----" << endl;
+    sliceArea = pizzaArea / 8;
+    cout << "The area of each slice is " << sliceArea << "." << endl;
 
 
 
