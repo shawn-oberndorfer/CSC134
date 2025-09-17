827adf8fca4a62c39e600bae91eaf6e3b42d134e
Shawn Oberndorfer <obesr2008@icloud.com>
2025-09-17 14:31:08 +0000

    Started M3T1

---
+++ M3T1.cpp
@@ -0,0 +1,33 @@
+/*
+Shawn Oberndorfer
+CSC 134
+M3T1
+9/17/25
+*/
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    double length1, length2, width1, width2;
+    double area1, area2;
+    cout << "Enter the length and width of two rectangles." << endl;
+    cout << "First rectangle." << endl;
+    cout << "Length: ";
+    cin >> length1;
+    cout << "Width: ";
+    cin >> width1;
+    cout << "Second rectangle." << endl;
+    cout << "Length: ";
+    cin >> length2;
+    cout << "Width: ";
+    cin >> width2;
+
+    area1 = length1 * width1;
+    area2 = length2 * width2;
+
+    cout << "The area of the first rectangle is " << area1 << " and the area of the second is " << area2 << "." << endl;
+
+    return 0;
+}
+
