08369e1a941ee11612fe77c98de316e7c5fa6043
Shawn Oberndorfer <obesr2008@icloud.com>
2025-09-22 14:40:52 +0000

    M3T2 Started

---
+++ M3T2.cpp
@@ -0,0 +1,34 @@
+/* 
+M3T2 - Craps Part 1
+CSC 134
+Shawn Oberndorfer
+Start Date 9/22/2025
+Last Worked On 9/22/2025
+*/
+
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int roll();
+
+int main()
+{
+    int num = roll();
+    cout << num << endl;
+
+
+
+    return 0;
+}
+
+int roll()
+{
+
+
+
+    return 6;
+}
