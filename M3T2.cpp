09ae480c252df8d100c140156e7e88c651763fc5
Shawn Oberndorfer <obesr2008@icloud.com>
2025-09-22 15:33:18 +0000

    Finished M3T2 for now

--- M3T2.cpp
+++ M3T2.cpp
@@ -5,8 +5,6 @@ Shawn Oberndorfer
 Start Date 9/22/2025
 Last Worked On 9/22/2025
 */
-
-
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
@@ -15,20 +13,48 @@ using namespace std;
 
 int roll();
 
+
 int main()
 {
-    int num = roll();
-    cout << num << endl;
+    //int num = roll();
+    //cout << num << endl;
+    int roll1;
+    int roll2;
+    int point;
+    bool gameEnd;
+    bool rollValid = true;
+    
+    srand(time(0));
 
+    roll1 = roll();
+    roll2 = roll();
+    int sum = roll1 + roll2;
 
+    cout << "ROLL: " << sum << endl;
+
+    if (sum == 7 || sum == 11){
+        cout <<"You win!" << endl;
+        gameEnd = true;
+    }
+    else if(sum == 2 || sum == 3 || sum == 12){
+        cout << "You Lose" << endl;
+        gameEnd = true;
+    } 
+    else if(sum == 4 || sum == 5 || sum == 6 || sum == 8 || sum == 9 || sum == 10){
+            point = sum;
+            cout << "Your point is: " << point << endl;
+            gameEnd = false;
+    }
+    else if(sum > 12 || sum < 2){
+        cout << "Invalid roll number is too high or too low. (Total of both rolls must be 2-12.)" << endl;
+        rollValid = false;
+    }
 
     return 0;
 }
 
-int roll()
-{
-
-
-
-    return 6;
+int roll(){
+    int myRoll;
+    myRoll = (rand() % 6) + 1;
+    return myRoll;
 }
