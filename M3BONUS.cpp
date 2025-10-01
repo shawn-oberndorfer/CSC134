4e16d953034e6447eeb81754b25ba2e879b808ba
Shawn Oberndorfer <obesr2008@icloud.com>
2025-10-01 15:33:11 +0000

    Added several enemies that can be fought via a struct.

--- M3BONUS.cpp
+++ M3BONUS.cpp
@@ -1,7 +1,6 @@
 /*
 Shawn Oberndorfer / Copilot
 10/1/2025
-
 */
 #include <iostream>
 #include <cstdlib>
@@ -11,7 +10,15 @@ Shawn Oberndorfer / Copilot
 #include <limits>
 using namespace std;
 
-void printStatus(int playerHP, int enemyHP) {
+// Enemy definition
+struct Enemy {
+    string name;
+    int maxHP;
+    int minDamage;
+    int maxDamage;
+};
+
+void printStatus(int playerHP, int enemyHP, string enemyName) {
     cout << "\n--- Status ---\n";
     cout << "Player HP: " << playerHP << "\n";
     cout << "Enemy  HP: " << enemyHP << "\n";
@@ -28,7 +35,7 @@ void waitForEnter() {
     cin.get(); // actually wait for Enter
 }
 
-void playerTurn(int& enemyHP, bool& isDefending) {
+void playerTurn(int& enemyHP, bool& isDefending, string enemyName) {
     cout << "\nYour turn!\n";
     cout << "Choose an action:\n";
     cout << "1) Attack\n";
@@ -40,11 +47,11 @@ void playerTurn(int& enemyHP, bool& isDefending) {
 
     if (choice == 1) {
         int damage = (rand() % 4) + 2; // random 2–5
-        cout << "\nYou strike the enemy for " << damage << " damage!\n";
+        cout << "You strike the " << enemyName << " for " << damage << " damage!\n";
         enemyHP -= damage;
         enemyHP = clampToZero(enemyHP);
         isDefending = false;
-        waitForEnter();
+        waitForEnter(); // pause until player presses Enter
     } 
     else if (choice == 2) {
         cout << "You brace yourself for the enemy's attack!\n";
@@ -56,9 +63,10 @@ void playerTurn(int& enemyHP, bool& isDefending) {
     }
 }
 
-void enemyTurn(int& playerHP, bool& isDefending) {
-    cout << "\nEnemy's turn...\n";
-    int damage = (rand() % 3) + 1; // random 1–3
+void enemyTurn(int& playerHP, bool& isDefending, Enemy currentEnemy) {
+    cout << "\n" << currentEnemy.name << "'s turn...\n";
+    int damage = (rand() % (currentEnemy.maxDamage - currentEnemy.minDamage + 1))
+                 + currentEnemy.minDamage;
 
     if (isDefending) {
         cout << "Your defense absorbs some of the blow!\n";
@@ -66,14 +74,25 @@ void enemyTurn(int& playerHP, bool& isDefending) {
         isDefending = false;
     }
 
-    cout << "The enemy hits you for " << damage << " damage!\n";
-    this_thread::sleep_for(chrono::seconds(2)); // 2-second pause
+    cout << currentEnemy.name << " hits you for " << damage << " damage!\n";
     playerHP -= damage;
     playerHP = clampToZero(playerHP);
+
+    // Random pause between 1 and 3 seconds
+    int pauseTime = (rand() % 2) + 1;
+    this_thread::sleep_for(chrono::seconds(pauseTime));
 }
 
 int main() {
     srand(time(0)); // seed randomness once
+
+    // Enemy list
+    Enemy enemies[] = {
+        {"Slime", 8, 1, 3},
+        {"Goblin", 12, 2, 4},
+        {"Wolf", 10, 1, 5}
+    };
+    int numEnemies = sizeof(enemies) / sizeof(enemies[0]);
 
     char playAgain = 'y';
 
@@ -81,19 +100,26 @@ int main() {
         cout << "\n=== New Battle Begins! ===\n";
 
         int playerHP = 10;
-        int enemyHP = 8;
         bool isDefending = false;
 
+        // Pick a random enemy
+        int enemyIndex = rand() % numEnemies;
+        Enemy currentEnemy = enemies[enemyIndex];
+        int enemyHP = currentEnemy.maxHP;
+
+        cout << "\nA wild " << currentEnemy.name << " appears!\n";
+
+        // Battle loop
         while (playerHP > 0 && enemyHP > 0) {
-            printStatus(playerHP, enemyHP);
-            playerTurn(enemyHP, isDefending);
+            printStatus(playerHP, enemyHP, currentEnemy.name);
+            playerTurn(enemyHP, isDefending, currentEnemy.name);
 
             if (enemyHP == 0) {
-                cout << "\nThe enemy falls. You win!\n";
+                cout << "\nThe " << currentEnemy.name << " falls. You win!\n";
                 break;
             }
 
-            enemyTurn(playerHP, isDefending);
+            enemyTurn(playerHP, isDefending, currentEnemy);
 
             if (playerHP == 0) {
                 cout << "\nYou collapse. Game over.\n";
