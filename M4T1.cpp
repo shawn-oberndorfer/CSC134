3b15e2940cc91fd42483ffbc3310bbb0bf93244e
Shawn Oberndorfer <obesr2008@icloud.com>
2025-10-12 19:15:30 +0000

    Started and finished M4T1

---
+++ M4T1.cpp
@@ -0,0 +1,17 @@
+/*
+Shawn Oberndorfer
+M4T1
+10/12/2025
+Simple Hello program
+*/
+#include <iostream>
+using namespace std;
+
+int main(){
+    int i = 0;
+    while (i < 5){
+        cout << "Hello" << endl;
+        i++;
+    }
+        return 0;
+}
