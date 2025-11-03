26f37dbe345445d5bb55f1210855c61e35a4c2ca
Shawn Oberndorfer <obesr2008@icloud.com>
2025-11-03 15:18:11 +0000

    Completed the Imported Prof Norris code to finish.

---
+++ Module 5/M5LAB2.cpp
@@ -0,0 +1,45 @@
+/*
+CSC 134
+M5LAB2
+Shawn Oberndorfer
+11/3/2025
+*/
+// M5LAB2
+// Header goes here
+
+#include <iostream>
+using namespace std;
+
+// Declare (Write the prototypes for)
+// the getLength,
+// getWidth, getArea, and displayData
+// functions here.
+
+int main()
+{
+	// This program calculates the area of a rectangle.
+	// TODO: fix any syntax errors
+	
+   double length,    // The rectangle's length
+          width,     // The rectangle's width
+          area;      // The rectangle's area
+          
+   // Get the rectangle's length.
+   length = getLength();
+   
+   // Get the rectangle's width.
+   width = getWidth()
+   
+   // Get the rectangle's area.
+   are = getArea();
+   
+   // Display the rectangle's data.
+   displayData(length, width, area);
+          
+   return 0;
+}
+
+//***************************************************
+// TODO: write the getLength, getWidth, getArea,    *
+// and displayData functions below.                 *
+//***************************************************
