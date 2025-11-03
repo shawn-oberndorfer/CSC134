f2ea9103ba6d1818542d1920e79f573c06335bcd
Shawn Oberndorfer <obesr2008@icloud.com>
2025-11-03 15:22:54 +0000

    I added the missing functions and the program properly works.

--- Module 5/M5LAB2.cpp
+++ Module 5/M5LAB2.cpp
@@ -4,34 +4,28 @@ M5LAB2
 Shawn Oberndorfer
 11/3/2025
 */
-// M5LAB2
-// Header goes here
-
 #include <iostream>
 using namespace std;
 
-// Declare (Write the prototypes for)
-// the getLength,
-// getWidth, getArea, and displayData
-// functions here.
+int getLength();
+int getWidth();
+int getArea(int, int);
+void displayData(int, int, int);
 
 int main()
 {
-	// This program calculates the area of a rectangle.
-	// TODO: fix any syntax errors
-	
-   double length,    // The rectangle's length
-          width,     // The rectangle's width
-          area;      // The rectangle's area
+	double length,    // The rectangle's length
+           width,     // The rectangle's width
+           area;      // The rectangle's area
           
    // Get the rectangle's length.
    length = getLength();
    
    // Get the rectangle's width.
-   width = getWidth()
+   width = getWidth();
    
    // Get the rectangle's area.
-   are = getArea();
+   area = getArea(length, width);
    
    // Display the rectangle's data.
    displayData(length, width, area);
@@ -39,7 +33,25 @@ int main()
    return 0;
 }
 
-//***************************************************
-// TODO: write the getLength, getWidth, getArea,    *
-// and displayData functions below.                 *
-//***************************************************
+int getLength(){
+    int length; 
+    cout << "Enter the length of your rectangle.\n";
+    cin >> length;
+    return length;
+}
+int getWidth(){
+    int width; 
+    cout << "Enter the width of your rectangle.\n";
+    cin >> width;
+    return width;
+}
+int getArea(int length, int width){
+    int area;
+    area = length * width;
+    return area;
+}
+void displayData(int length, int width, int area){
+    cout << "If Length: " << length;
+    cout << "\nAnd Width: " << width;
+    cout << "\nThen Area is: " << area << endl;
+}
