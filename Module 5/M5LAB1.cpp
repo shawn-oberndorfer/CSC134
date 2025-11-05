349da3598bf75eee779159eecd2a8b28d9ece2ea
Shawn Oberndorfer <obesr2008@icloud.com>
2025-11-05 16:01:42 +0000

    Added some story content.

--- Module 5/M5LAB1.cpp
+++ Module 5/M5LAB1.cpp
@@ -10,15 +10,37 @@ using namespace std;
 
 int GetChoice(int);
 int AdventureStart();
+void YouLose();
 
 
 int main(){
     int storyChoice;
+    bool hasLost = false;
 
     storyChoice = AdventureStart();
 
     cout << storyChoice;
 
+    switch(storyChoice){
+        case 1:
+            cout << "\nYou walk up to the front of the tower and push the doors open revealing the magnificent interior to you.";
+            break;
+        case 2:
+            cout << "\nYou see a ladder leading to a high up window around the bock of the tower.\n As you climb the ladder up to the window a gust of wind knocks you off and land on your arm, breaking it.";
+            cout << "\nYou head back home and hope that people don't ask how you injured yourself\n\n";
+            hasLost = true;
+            break;
+        case 3:
+            cout << "\nYou go home to be shamed by your fellow adventurers for your cowardice.\n";
+            hasLost = true;
+            break;
+    }
+    if(hasLost == false){
+        //story continues here
+    } 
+    else{
+        YouLose();
+    }
 
     return 0;
 }
@@ -41,7 +63,7 @@ int AdventureStart(){
     int storyChoice;
 
     cout << "Story Text\n";
-    cout << "Choice 1, 2, and 3.\n";
+    cout << "1: Enter the tower through the front door.\n2: Search around the tower for another entrance\n3: Go home. Exploring this tower would be too much work.\n";
     maxChoice = 3;
     choice = GetChoice(maxChoice);
     switch(choice){
@@ -58,9 +80,14 @@ int AdventureStart(){
             storyChoice = 3;
             break;
         default:
-            cout << "Hey you shouldn't be able to see this text.\n";
+            cout << "Hey! You shouldn't be able to see this text.\n";
     }
 
 
     return storyChoice;
+}
+
+void YouLose(){
+    cout << "---GAME OVER---\n";
+    cout << "---YOU LOSE---\n";
 }
