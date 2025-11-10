a8c1fb1bb5dd61f35bbb6cc723cccf9b556b73e3
Shawn Oberndorfer <obesr2008@icloud.com>
2025-11-10 15:56:28 +0000

    Added Days of week to M6T1 and created M6LAB1 File

--- Module 6/M6T1.cpp
+++ Module 6/M6T1.cpp
@@ -40,9 +40,16 @@ void part2(){
     int averageCars;
 
     int carsPast[maxDays];
+    string daysOfWeek[maxDays];
+
+    daysOfWeek[0] = "Monday";
+    daysOfWeek[1] = "Tuesday";
+    daysOfWeek[2] = "Wednesday";
+    daysOfWeek[3] = "Thursday";
+    daysOfWeek[4] = "Friday";
 
     for(int i = 1; i <= maxDays; i++){
-        cout << "How many cars past on day " << i << ": ";
+        cout << "How many cars past on " << daysOfWeek[i-1] << ": ";
         cin >> carsPast[i-1];
         totalCars += carsPast[i-1];
         cout  << endl;
@@ -51,7 +58,7 @@ void part2(){
     averageCars = totalCars / maxDays;
     
     for(int j = 1; j <= maxDays; j++){
-        cout << "On day " << j << ": " << carsPast[j-1] << " cars past" << endl;
+        cout << "On " << daysOfWeek[j-1] << ": " << carsPast[j-1] << " cars past" << endl;
     }
     cout << "\nTotal Cars: " << totalCars << "\nAverage Cars Per Day: " << averageCars << endl;;
 
