e5f0b7c041e4dbf82f1fa6955d83e6edc82dbe15
Shawn Oberndorfer <obesr2008@icloud.com>
2025-12-01 15:42:34 +0000

    Started writing ideas for final project. Mostly completed M7T1

---
+++ Module 7/M7T1.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+// Next time we'll put the class in a separate file
+class Restaurant {
+  private:
+    string name;    // the name
+    double rating;  // 0 to 5 stars
+
+  public:
+	// constructor 
+	Restaurant(string n, double r) {
+		name = n;
+		rating = r;
+	}
+	// getters and setters
+    void setName(string n) {
+        name = n; 
+    }
+    void setRating(double r) {
+
+        if (r >= 0 && r <= 5) {
+            rating = r;
+        }
+    }
+    string getName() const {
+        return name;
+    }
+    double getRating() const {
+        return rating;
+    }
+  
+
+    void printInfo() {
+        cout << "Restaurant: " << name << ", \nRating: " << rating << " stars" << endl;
+    }
+};
+
+int main() {
+    cout << "M7T1 - Restaurant Reviews" << endl;
+
+
+
+    Restaurant lunch = Restaurant("Zaxbys", 4.0);
+    Restaurant dinner = Restaurant("Olive Garden", 4.5);
+
+
+    lunch.printInfo();
+    dinner.printInfo();
+
+    return 0;
+
+}
